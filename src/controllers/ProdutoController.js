4e466baa784a1f4a89e246f6ce241f169c190494
GHCMelo <gabcardosomelo@gmail.com>
2020-11-23 18:23:54 -0300

    Teste

---
+++ src/controllers/ProdutoController.js
@@ -0,0 +1,32 @@
+const { response } = require('../app');
+const db = require('../config/database');
+
+exports.createProduto = async(req, res) => {
+    const { nome, descricao, valor, categoriaProduto } = req.body
+    const { rows } = db.query(
+        "INSERT INTO produto (nome, descricao, valor, categoriaProdutoId) VALUES ($1, $2, $3, $4)", [nome, descricao, valor, categoriaProduto]
+    )
+
+    res.status(201).send({
+        msg: "Produto adicionado com sucesso",
+        produto: {
+            nome,
+            descricao,
+            valor,
+            categoriaProduto
+        }
+    })
+}
+
+exports.listProdutoByCategoria = async(req, res) => {
+    const { idCategoria } = req.params;
+    const response = db.query(
+        "SELECT * FROM produto as A INNER JOIN produtoCategoria as B ON A.categoriaprodutoId = B.id WHERE B.ID = ($1)", [idCategoria], (err, res) =>{
+            console.log(res);
+        }
+    )
+
+    res.status(200).send({
+        Produtos: response.rows
+    })
+}
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const CategoriaController = require('./controllers/CategoriaController')
+const ProdutoController = require('./controllers/ProdutoController')
 
 router.post('/categoria', CategoriaController.createProdutoCategoria)
+router.post('/produto', ProdutoController.createProduto)
+router.get('/produto/:idCategoria', ProdutoController.listProdutoByCategoria)
 
 module.exports = router
 
