e1798bbbbda099eee2965a531e9f4e11f6258e44
GHCMelo <gabcardosomelo@gmail.com>
2020-11-23 19:48:01 -0300

    a

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -23,14 +23,10 @@ exports.listAllCategories = async (req, res) => {
 
 exports.listById = async (req, res) => {
     const id = req.params
-    try {
-        const response = await db.query(
+    console.log(id);
+    const response = await db.query(
         "SELECT * FROM produtoCategoria WHERE id = ($1)", [id]
-        )
-        return response
-    } catch(e){
-        console.log(e)
-    }
+    )
 
     res.status(200).send({
         Categoria: response.rows
